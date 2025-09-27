703f9cbd1b1a249a976947fc99dd7700eee7bbdc
dalesgames167 <cwriterfantasypeaced@gmail.com>
2025-09-27 12:31:38 -0400

    Variables

---
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GeoController : MonoBehaviour
+{
+    string variable1 = "Hello";
+    string variable2 = "World";
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Debug.Log(variable1 + variable2);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
